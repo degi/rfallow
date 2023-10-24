c111280e9da1b3502816283cc1cf43d0f9414817
Degi Harja Asmara <degi.ecomodels@gmail.com>
2023-10-24 16:05:01 +0800

    add auto install lib

--- ui.R
+++ ui.R
@@ -9,7 +9,31 @@ library(shinyjs)
 library(excelR)
 library(markdown)
 
-# source("global.R")
+
+install_load <- function (package1, ...)  {   
+  
+  # convert arguments to vector
+  packages <- c(package1, ...)
+  
+  # start loop to determine if each package is installed
+  for(package in packages){
+    
+    # if package is installed locally, load
+    if(package %in% rownames(installed.packages()))
+      do.call('library', list(package))
+    
+    # if package is not installed locally, download, then load
+    else {
+      install.packages(package)
+      do.call("library", list(package))
+    }
+  } 
+}
+
+install_load("shinyjs", "stars", "shinydashboardPlus", "shinyWidgets", 
+             "openxlsx2", "RColorBrewer", "areaplot", "mapview", "leaflet",
+             "leafem", "dplyr", "reshape", "randomcoloR", "shinyjqui",
+             "excelR", "fresh", "markdown")
 
 mytheme <- create_theme(
   adminlte_color(
